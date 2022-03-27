884f2986d582546dff1f2718752d3804120d08de
Jan Kociniak <jan.kociniak@gmail.com>
2022-03-27 20:04:53 +0200

    bugfix, correct results

--- hyperdist/combine_results.py
+++ hyperdist/combine_results.py
@@ -10,10 +10,34 @@ def parse_args():
     return args.multirun_dir
 
 
+# https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
+
+
+def handle_list(s):
+    # we assume that the string is of form xxxx[xxx]xxxx[xxxx]x[xxxx]xxxxx
+    left_idx = find_all(s, '[')
+    right_idx = find_all(s, ']')
+    par_idx = zip(left_idx, right_idx)
+    s = list(s)
+    for lp_idx, rp_idx in par_idx:
+        substr = ''.join(s[lp_idx:rp_idx+1])
+        s[lp_idx:rp_idx+1] = list(substr.replace(',', '|'))
+    return ''.join(s)
+
+
 def get_experiment_folders(multirun_dir):
     for base_entry in os.scandir(multirun_dir):
         if base_entry.is_dir():  # subfolder with name param1=val1,param2=val2 etc
-            params = base_entry.name.split(sep=',', maxsplit=1)
+            params = base_entry.name
+            params = handle_list(params)
+            params = params.split(sep=',')
             params = [p.split(sep='=') for p in params]
             params = {p[0]: p[1] for p in params}
             for seed_entry in os.scandir(base_entry.path):
