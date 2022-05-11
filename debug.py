b97d865e0c5360d98d2cc5377804897f5a907876
Jan Kociniak <jk394348@students.mimuw.edu.pl>
2022-05-11 20:49:01 +0200

    debuggable runner

--- debug.py
+++ debug.py
@@ -1,4 +1,8 @@
+### VERSION OF RUN.PY WITHOUT HYDRA DECORATOR
+### COMPOSE API IS USED INSTEAD TO ALLOW INTERACTIVE DEBUGGING
+
 import hydra
+from hydra import initialize, compose
 from hydra.utils import get_original_cwd
 from omegaconf import DictConfig
 from config import run_training
@@ -9,12 +13,12 @@ import pickle
 import threading
 
 
-@hydra.main(config_path='conf', config_name='config')
 def train(cfg: DictConfig) -> None:
-    print("Working directory : {}".format(os.getcwd()))
+    cwd = os.getcwd()
+    print("Working directory : {}".format(cwd))
 
     dataset_path = build_dataset_path(**cfg['dataset_params'])
-    dataset_path = os.path.join(get_original_cwd(), 'datasets', dataset_path)
+    dataset_path = os.path.join(cwd, 'datasets', dataset_path)
 
     with open(dataset_path, 'rb') as f:
         datasets = pickle.load(f)
@@ -27,11 +31,13 @@ def train(cfg: DictConfig) -> None:
     base_dir = os.getcwd()
     setup_dirs(folder_names, base_dir)
 
-    dump_thread = threading.Thread(target=dump_results, args=(datasets, results, base_dir, run, cfg['plots']))
-    dump_thread.start()
-
 
 if __name__ == "__main__":
-    train()
+    overrides = [
+        'epochs=5'
+    ]
 
-
+    with initialize(config_path="conf"):
+        cfg = compose(config_name="config.yaml", overrides=overrides)
+    
+    train(cfg)
